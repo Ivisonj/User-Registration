69dd83341401515e09bf89a84b26539da213e8d8
Ivisonj <ivison_j@outlook.com>
2023-02-24 17:05:01 -0300

    implementação da tabela e função de exclir

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -2,16 +2,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import React from "react";
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 import Logo from '../components/template/Logo/Logo';
 import Nav from '../components/template/Nav/Nav';
-import Home from '../components/home/Home';
+import Routes from './routes/Routes';
 import Footer from '../components/template/Footer/Footer';
 
 export default props =>
-    <div className="app">
-        <Logo/>
-        <Nav/>
-        <Home/>
-        <Footer/>
-    </div>
+    <BrowserRouter>
+        <div className="app">
+            <Logo/>
+            <Nav/>
+            <Routes />
+            <Footer/>
+        </div>
+    </BrowserRouter>
